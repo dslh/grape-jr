2e1bc4c0dbdfd09781e87334b574f7602f2b6d2c
Doug Hammond <d.lakehammond@gmail.com>
2017-01-29 17:09:10 +1300

    Value formatter for dates.

---
+++ lib/grape/jsonapi/value_formatters/iso_date_value_formatter.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Formats dates in ISO 8601, YYYY-mm-dd format
+#
+class IsoDateValueFormatter < JSONAPI::ValueFormatter
+  class << self
+    def format(raw_value)
+      raw_value.iso8601
+    end
+
+    def unformat(formatted)
+      Date.iso8601 formatted
+    end
+  end
+end
